d5cc1451785ae169b5a6f3405cf3e5201d19d5ab
Barada Prasanna Pradhan <baradaprasanna03@gmail.com>
2022-03-27 17:04:06 +0530

    Create Full Pyramid pattern of stars.cpp

---
+++ Full Pyramid pattern of stars.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, space, j;
+    for(i=1; i<=4; i++)
+    {
+        for(space=4; space>i; space--)
+            cout<<" ";
+        for(j=0; j<i; j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
